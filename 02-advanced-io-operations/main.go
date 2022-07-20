3a3a09a26409f1b4a5166209856f351d9f98ce84
laureanray <laureanraybahala@gmail.com>
2022-07-21 00:55:16 +0800

    list and count files

--- 02-advanced-io-operations/main.go
+++ 02-advanced-io-operations/main.go
@@ -3,10 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
+// TODO: Maybe fun to implement this with goroutines?
+// TODO: Or implement cool algo to improve performance in really large directories
 func main() {
-	wd, err := os.Getwd()
+	if len(os.Args) != 2 {
+		fmt.Println("Please specify a path.")
+		return
+	}
 
-	fmt.Println(wd, err)
+	root, err := filepath.Abs(os.Args[1]) // Get absolute path
+
+	if err != nil {
+		fmt.Println("Cannot get absolute path: ", err)
+	}
+
+	fmt.Println("Listing files in", root)
+
+	var c struct {
+		files int
+		dirs  int
+	}
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		// Walk the tree to count file and folders
+		if info.IsDir() {
+			c.dirs++
+		} else {
+			c.files++
+		}
+
+		fmt.Println("-", path)
+		return nil
+	})
+
+	fmt.Printf("Total: %d files in %d directories", c.files, c.dirs)
 }
