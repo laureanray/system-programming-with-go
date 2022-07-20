f023b26242759beee16d9daa999edf3e65104d98
laureanray <laureanraybahala@gmail.com>
2022-07-21 01:00:39 +0800

    restructure code

---
+++ 02-advanced-io-operations/impl/lister.go
@@ -0,0 +1,43 @@
+package impl
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// TODO: Maybe fun to implement this with goroutines?
+// TODO: Or implement cool algo to improve performance in really large directories
+func ListDir(path string) {
+	if path == "" {
+		fmt.Println("Please specify a path.")
+		return
+	}
+
+	root, err := filepath.Abs(os.Args[1]) // Get absolute path
+
+	if err != nil {
+		fmt.Println("Cannot get absolute path: ", err)
+	}
+
+	fmt.Println("Listing files in", root)
+
+	var c struct {
+		files int
+		dirs  int
+	}
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		// Walk the tree to count file and folders
+		if info.IsDir() {
+			c.dirs++
+		} else {
+			c.files++
+		}
+
+		fmt.Println("-", path)
+		return nil
+	})
+
+	fmt.Printf("Total: %d files in %d directories", c.files, c.dirs)
+}
